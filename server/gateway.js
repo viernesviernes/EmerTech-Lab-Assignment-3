fdf7907ef2335135bbe116d887dd6459711e8a4e
Moustafa-FD <moustafafadlalla@gmail.com>
2026-03-20 02:28:06 -0400

    gateway cookie reader/setter fix

--- server/gateway.js
+++ server/gateway.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const { ApolloServer } = require('@apollo/server');
-const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
 const { expressMiddleware } = require('@as-integrations/express4');
 
 async function startGateway() {
   const app = express();
 
   const gateway = new ApolloGateway({
-    supergraphSdl: new IntrospectAndCompose({
-      subgraphs: [
-        { name: 'auth', url: 'http://localhost:3003/graphql' },
-      ],
-    }),
+    serviceList: [
+      { name: 'auth', url: 'http://localhost:3003/graphql' },
+    ],
+    buildService({ name, url }) {
+      return new RemoteGraphQLDataSource({
+        url,
+        willSendRequest({ request, context }) {
+          // Forward only the 'token' cookie from client to subgraph
+          const tokenCookie = context.req?.cookies?.token;
+          if (tokenCookie) {
+            request.http.headers.set('cookie', `token=${tokenCookie}`);
+          }
+        },
+        didReceiveResponse({ response, context }) {
+          // Forward Set-Cookie headers from subgraph to client
+          const setCookieHeaders = response.http.headers.get('set-cookie');
+          if (setCookieHeaders && context.res) {
+            context.res.set('set-cookie', setCookieHeaders);
+          }
+          return response;
+        },
+      });
+    },
   });
 
   const server = new ApolloServer({
@@ -21,11 +40,14 @@ async function startGateway() {
 
   await server.start();
 
+  app.use(cookieParser());
   app.use(
     '/graphql',
     cors(),
     express.json(),
-    expressMiddleware(server)
+    expressMiddleware(server, {
+      context: ({ req, res }) => ({ req, res }),
+    })
   );
 
   app.listen(4000, () => {
